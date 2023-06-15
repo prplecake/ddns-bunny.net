0f64fe20a4e15d5684196840c949b6c2b0e561d1
prplecake <me@prplecake.com>
2023-06-15 18:32:58 -0500

    Add logging

--- src/BunnyDDNS.CLI/Program.cs
+++ src/BunnyDDNS.CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BunnyDDNS.CLI.Api.Bunny;
 using BunnyDDNS.Core.Configuration;
 using BunnyDDNS.Core.Utilities;
+using Serilog;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -8,14 +9,31 @@ namespace BunnyDDNS.CLI;
 
 public static class Program
 {
+    private static bool _debug;
+    private static ILogger? _logger;
     public static int Main()
     {
+        // Setup logging
+        var logOutputTemplate =
+            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}";
+        var logConfig = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(
+                outputTemplate: logOutputTemplate
+            );
+        if (_debug) logConfig.MinimumLevel.Debug();
+        Log.Logger = logConfig.CreateLogger();
+        _logger = Log.ForContext(typeof(Program));
+
         // Read config
+        _logger.Debug("Getting configuration");
         var config = SetupConfiguration();
 
         // Get current IP address
         var _ipUtils = new IpUtils();
         string currentIp = _ipUtils.GetCurrentIp();
+        _logger.Information("Current IP: {Ip}", currentIp);
 
         // Find record in Bunny DNS
         var dnsClient = new DnsApiClient(config);
