41d88b88a3bb3314dff6fee2e6d12ac696836a87
Matty Jorgensen <me@prplecake.com>
2023-08-22 20:29:01 -0500

    Add Config tests

---
+++ tests/BunnyDDNS.Core.Tests/Entities/Configuration/ConfigTests.cs
@@ -0,0 +1,32 @@
+using BunnyDDNS.Core.Configuration;
+
+namespace BunnyDDNS.Core.Tests.Entities.Configuration;
+
+[TestClass]
+public class ConfigTests
+{
+    [TestMethod]
+    public void Config_Tests()
+    {
+        // Arrange
+        var expectedAccessToken = "token123456";
+        var expectedRecordName = "ddns";
+        var expectedZoneName = "int.example.com";
+        var configObj = new Config
+        {
+            Bunny = new Bunny
+            {
+                AccessToken = expectedAccessToken
+            },
+            RecordName = expectedRecordName,
+            ZoneName = expectedZoneName
+        };
+
+        // Assert
+        Assert.IsNotNull(configObj);
+        Assert.IsNotNull(configObj.Bunny);
+        Assert.AreEqual(expectedAccessToken, configObj.Bunny.AccessToken);
+        Assert.AreEqual(expectedRecordName, configObj.RecordName);
+        Assert.AreEqual(expectedZoneName, configObj.ZoneName);
+    }
+}
